8eb7d6deb4bf8323e682a5d30a766798e7a3589a
James Hamann <jameshamann@Jamess-MacBook-Pro.local>
2017-11-28 00:12:36 +0000

    building parallax theme

---
+++ js/init.js
@@ -0,0 +1,4 @@
+// Initialize collapse button
+ $(".button-collapse").sideNav();
+ // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+ $('.collapsible').collapsible();
