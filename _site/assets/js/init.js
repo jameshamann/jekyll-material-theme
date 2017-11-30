4b2bdffe4bb5f1e1aef5300020b5c68cba9537d4
James Hamann <jameshamann@Jamess-MacBook-Pro.local>
2017-11-30 20:27:13 +0000

    adds more pics

--- _site/assets/js/init.js
+++
@@ -1,1 +0,0 @@
-!function(l){l(function(){l(".button-collapse").sideNav(),l(".parallax").parallax(),l(".collapsible").collapsible(),l(".carousel.carousel-slider").carousel({fullWidth:!0})})}(jQuery);
