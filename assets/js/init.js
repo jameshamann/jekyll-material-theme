7043796710f216cbfec0992e1c4ade6942138977
pmsbk <pim@sebok.nl>
2018-01-30 21:35:46 +0100

    Persistent cookie consent choice
    Stores in localstorage when a user dismisses the cookie message, so that it does not return at refresh.

--- assets/js/init.js
+++ assets/js/init.js
@@ -9,7 +9,12 @@
      $('.scrollspy').scrollSpy();
      $('.tap-target').tapTarget('open');
 
+     if (localStorage.getItem('cookieconsent') === 'true') {
+       $('#cookies').hide()
+     }
+
      jQuery('#cookies').on('click', function(event) {
+            localStorage.setItem('cookieconsent', 'true')
             jQuery('#cookies').toggle('hide');
        });
 
